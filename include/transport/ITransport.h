aa27dbb61730b085bea1ffbe08a16eabf443b4eb
Evyatar <evyatar.v@gmail.com>
2017-01-19 01:38:05 +0200

    add data (transport data)
    for http this pass the auth

--- include/transport/ITransport.h
+++ include/transport/ITransport.h
@@ -3,7 +3,7 @@
 
 typedef int(*sh_send_command)(const char* addr, size_t addr_len, const char* command, size_t command_len, const void* data, size_t data_len);
 
-typedef int(sh_send_configuration)(const char* addr, size_t addr_len, const void* config, size_t config_len);
+typedef int(sh_send_configuration)(const char* addr, size_t addr_len, const void* config, size_t config_len, const void* data, size_t data_len);
 
 
 typedef int(*sh_recv_data)(const char* addr, size_t addr_len, const char* command, size_t command_len, const void* data);
