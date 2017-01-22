9dcd80396dd0ae66651cf0311efc5d145ca9d4ed
Evyatar <evyatar.v@gmail.com>
2017-01-23 00:09:29 +0200

    WiFi reconnect
    While configuring WiFi we need to make sure the device will be allways be accessible at the end of the process.

--- sketch/ESP8266_http_server/ESP8266_http_server.ino
+++ sketch/ESP8266_http_server/ESP8266_http_server.ino
@@ -3,65 +3,121 @@
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
+#include <Ticker.h>
 
 MDNSResponder mdns;
 
+// wifi connection
+#define WIFI_STATUS_PULL_INTERVAL (500)
+#define WIFI_CONNECTION_TIMEOUT   (60)
+#define WIFI_MAX_SSID_LENGTH      (32)
+#define WIFI_MAX_PASSWORD_LENGTH  (32)
+
+// esp8266 gpios
+#define SONOFF_BUTTON    0
+#define SONOFF_RELAY    12
+#define SONOFF_LED      13
+#define SONOFF_INPUT    14
+
 // network credentials
-const char* SSID_NAME = "*******";
-const char* SSID_PASSWORD = "*******";
+const char* DEFAULT_SSID_NAME = "******";
+const char* DEFAULT_SSID_PASSWORD = "******";
+const char* CURRENT_SSID_NAME = DEFAULT_SSID_NAME;
+const char* CURRENT_SSID_PASSWORD = DEFAULT_SSID_PASSWORD;
 
 // http authentication 
-const char* USER = "*******";
-const char* USER_PASSWORD = "*******";
+const char* USER = "******";
+const char* USER_PASSWORD = "******";
 
 // return http page
 String RETURN_WEB_PAGE = "";
 
-int gpio13Led = 13;
-int gpio12Relay = 12;
+Ticker ticker;
 
 ESP8266WebServer server(80);
 
-void wifi_connect(const char* ssid, const char* password)
+void led_tick()
 {
-  WiFi.begin(ssid, password);
-  Serial.println("");
+  //toggle state
+  int state = digitalRead(SONOFF_LED);  // get the current state of GPIO1 pin
+  digitalWrite(SONOFF_LED, !state);     // set pin to the opposite state
+}
 
+bool wifi_try_connect(const char* ssid, const char* password)
+{
+  int timeout_counter = 0; 
+  
   // Set ESP to stasion mode
   WiFi.mode(WIFI_STA);
-  Serial.print("WIFI mode set to stasion.");
-  Serial.println("");
+  Serial.println("WIFI mode set to stasion.");
+  Serial.print("Connecteing to: ");
+  Serial.println(ssid);
+
+  // start wifi
+  WiFi.begin(ssid, password); 
 
   // Wait for connection
   while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
+    delay(WIFI_STATUS_PULL_INTERVAL);
+    if (++timeout_counter == WIFI_CONNECTION_TIMEOUT)
+      return false;
+
     Serial.print(".");
+    led_tick();
   }
 
   // print wifi connection data
   Serial.println("");
-  Serial.print("Connected to ");
-  Serial.println(ssid);
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
-  
+
+  // set device dns name
   if (mdns.begin("esp8266", WiFi.localIP())) {
     Serial.println("MDNS responder started");
   }
+
+  // save latest success connection
+  CURRENT_SSID_NAME = ssid;
+  CURRENT_SSID_PASSWORD = password;
+  
+  return true;
 }
 
-void setup(void){
-  // preparing GPIOs
-  pinMode(gpio13Led, OUTPUT);
-  digitalWrite(gpio13Led, HIGH);
+void wifi_connect(const char* ssid, const char* password)
+{
+  if (wifi_try_connect(ssid, password))
+    return; 
+
+  Serial.print("Fail to connect: ");
+  Serial.println(ssid);
+  Serial.println("Try connecting previous ssid");
   
-  pinMode(gpio12Relay, OUTPUT);
-  digitalWrite(gpio12Relay, HIGH);
+  if (wifi_try_connect(CURRENT_SSID_NAME, CURRENT_SSID_PASSWORD))
+    return;
+
+  Serial.print("Fail to connect: ");
+  Serial.println(ssid);
+  Serial.println("Try connecting default ssid");
+
+  if (wifi_try_connect(DEFAULT_SSID_NAME, DEFAULT_SSID_PASSWORD))
+    return;
+
+  //TODO: open device as AP if fail do restart
+}
+
+void setup(void)
+{
+  // preparing GPIOs
+  pinMode(SONOFF_LED, OUTPUT);
+  digitalWrite(SONOFF_LED, HIGH);
+  
+  pinMode(SONOFF_RELAY, OUTPUT);
+  digitalWrite(SONOFF_RELAY, HIGH);
  
   Serial.begin(115200); 
   delay(5000);
 
-  wifi_connect(SSID_NAME, SSID_PASSWORD); 
+  wifi_connect(DEFAULT_SSID_NAME, DEFAULT_SSID_PASSWORD); 
   
   server.on("/", [](){
     if(!server.authenticate(USER, USER_PASSWORD))
@@ -72,18 +128,29 @@ void setup(void){
     if(!server.authenticate(USER, USER_PASSWORD))
       return server.requestAuthentication();
     server.send(200, "text/html", RETURN_WEB_PAGE);
-    digitalWrite(gpio13Led, LOW);
-    digitalWrite(gpio12Relay, HIGH);
+    digitalWrite(SONOFF_LED, LOW);
+    digitalWrite(SONOFF_RELAY, HIGH);
     delay(1000);
   });
   server.on("/off", [](){
     if(!server.authenticate(USER, USER_PASSWORD))
-      return server.requestAuthentication();      
+      return server.requestAuthentication();     
     server.send(200, "text/html", RETURN_WEB_PAGE);
-    digitalWrite(gpio13Led, HIGH);
-    digitalWrite(gpio12Relay, LOW);
+    digitalWrite(SONOFF_LED, HIGH);
+    digitalWrite(SONOFF_RELAY, LOW);
     delay(1000); 
   });
+  server.on("/config", [](){
+    if(!server.authenticate(USER, USER_PASSWORD))
+      return server.requestAuthentication(); 
+    Serial.println("config starts ...");
+    String arg = server.arg("ip"); 
+    
+    Serial.println(arg.length());
+    server.send(200, "text/html", RETURN_WEB_PAGE);
+    delay(1000); 
+  });
+  
   server.begin();
   Serial.println("HTTP server started");
 }
