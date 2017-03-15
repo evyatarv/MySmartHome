e1d3e52696bedcd69e315be6274cfe71a576b021
Evyatar <evyatar.v@gmail.com>
2017-03-15 22:41:23 +0200

    Fix saving sonoff_dual relays state

--- Sketch/d2_sonoff_dual.ino
+++ Sketch/d2_sonoff_dual.ino
@@ -13,28 +13,32 @@ void sonoff_dual_prepare_serial()
 
 void sonoff_dual_relay(int index, int cmd)
 {
+  byte relay_tmp_status = g_sonoff_dual_relay_status; 
+  
   if (cmd == relay_on)
-    SET_BIT(g_sonoff_dual_relay_status, index);
+    SET_BIT(relay_tmp_status, index);
   else if (cmd == relay_off)
-    CLEAR_BIT(g_sonoff_dual_relay_status, index);
+    CLEAR_BIT(relay_tmp_status, index);
   else
     Serial.println("SONOFF DUAL WRONG COMMAND");
 
   // DEBUG - remove
   Serial.print("SONOFF DUAL COMMAND: ");
-  Serial.println(g_sonoff_dual_relay_status);
+  Serial.println(relay_tmp_status);
   
   //DO NOT change or remove this check!
   //this is to make sure that both relays will NOT be activate together at all times (even if the enum has changed)
   //it can burn the shutter motor if both relays will work!!!
-  if (g_sonoff_dual_relay_status <= 0x02 && g_sonoff_dual_relay_status >= 0x00 )//check that the values are correct, meaning only ONE or TWO  or NONE
+  if (relay_tmp_status <= 0x02 && relay_tmp_status >= 0x00 )//check that the values are correct, meaning only ONE or TWO  or NONE
   {
-    /* 
+   /* 
    * first close the relays to be on the safe 
    * side that the will not be activate together 
    */
     sonoff_dual_send_relay_cmd(0);              
     sonoff_dual_send_relay_cmd(g_sonoff_dual_relay_status);
+
+    g_sonoff_dual_relay_status = relay_tmp_status;
   }
   else
   {
